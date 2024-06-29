dc4cf246409ebcdde3b78d487bcf2045cd70c6ff
Denis Popkov <pkvdenis@gmail.com>
2024-06-29 23:00:46 +0300

    lessons: move lessons projects to sub-folder.

---
+++ lessons/settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    plugins {
+        val kotlinVersion: String by settings
+        kotlin("jvm") version kotlinVersion
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+rootProject.name = "lessons"
+
+include("hello")
+include("m1l1-first")
+include("m1l2-basic")
+include("m1l3-func")
+include("m1l4-oop")
+include("m1l5-dsl")
+include("m2l1-coroutines")
+include("m2l2-flows")
+include("m2l3-kmp")
+include("m2l4-1-interop")
+include("m2l4-2-jni")
